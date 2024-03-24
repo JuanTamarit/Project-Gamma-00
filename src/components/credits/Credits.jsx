9aa39618daac1ba6872585314dae8b941b1ffa0c
JuanTamarit <116755072+JuanTamarit@users.noreply.github.com>
2024-03-24 18:31:17 +0100

    Créditos

--- src/components/credits/Credits.jsx
+++ src/components/credits/Credits.jsx
@@ -4,11 +4,11 @@ function Credits() {
   return (
     <>
       <div className="credits text-small">
-        <span>DISEÑO Y DESARROLLO:</span>
-        <span>JUAN TAMARIT</span>
-        <span>FRAMEWORK:</span>
-        <span>REACT.JS</span>
-        <span>VERSIÓN 2.0</span>
+        <span>Web diseñada y desarrollada por:</span>
+        <span>Juan Tamarit</span>
+        <span>Framework:</span>
+        <span>React.js</span>
+        <span>Versión 2.0</span>
       </div>
     </>
   );
