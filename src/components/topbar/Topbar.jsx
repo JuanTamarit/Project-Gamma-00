6379d660159cbde07651d221468607f353b930a4
JuanTamarit <116755072+JuanTamarit@users.noreply.github.com>
2024-03-25 13:45:02 +0100

    Enlace contacto Topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,22 +1,27 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import "./Topbar.scss";
 import Mail from "../icons/mail/mail";
-import PortafolioIcon from "../icons/portafolio/portafolioIcon";
-// import "src/scss/_variables.scss";
 
 function Topbar(props) {
+  const handleMailClick = () => {
+    const emailAddress = "tamaritvaju@gmail.com";
+    const subject = "JUANTAMARIT.COM - Motivo de contacto:";
+    const mailtoLink = `mailto:${emailAddress}?subject=${encodeURIComponent(
+      subject
+    )}`;
+
+    window.location.href = mailtoLink;
+  };
+
   return (
-    <>
-      <div className="topbar h6">
-        {/* <div className="topbar__nav"> */}
-        <Link>
-          <Mail color="#2d00f6" />
-          Contacto
-        </Link>
-        <Link to={props.url}>{props.link}</Link>
-        {/* </div> */}
-      </div>
-    </>
+    <div className="topbar h6">
+      <Link to="#" onClick={handleMailClick}>
+        <Mail color="#2d00f6" />
+        Contacto
+      </Link>
+      <Link to={props.url}>{props.link}</Link>
+    </div>
   );
 }
 
