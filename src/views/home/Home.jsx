aee219698929e14694470f247f561451a678efcb
JuanTamarit <116755072+JuanTamarit@users.noreply.github.com>
2024-04-09 16:44:15 +0200

    Bio text

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -61,7 +61,9 @@ export default function Home() {
               la comunicación, puesto que me atraen todas sus vertientes. Quizás
               por haber sido influenciado por un entorno familiar artístico y
               diverso, repleto de artes escénicas y plásticas, y por mi faceta
-              como músico profesional, previa a mi camino como diseñador.
+              como músico profesional, previa a mi camino como diseñador. Ambas
+              cosas, me permiten plantear propuestas de diseño inmersivas y con
+              un toque diferencial importante.
             </p>
             <br />
             <p>
